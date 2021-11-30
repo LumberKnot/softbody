b8251d1e66e9da700a797531c834e6f9bbc27148
LumberKnot <jens.lucas@gmail.com>
2021-11-30 13:47:51 +0100

    finn .gitignore

--- build.sbt
+++ build.sbt
@@ -1,1 +1,2 @@
-scalaVersion := "3.0.2"
+scalaVersion := "3.1.0"
+libraryDependencies += "se.lth.cs" %% "introprog" % "1.3.1"
--- vector2.scala
+++ vector2.scala
@@ -1,26 +1,41 @@
 package utilities
 
-case class Vector2(x: Float, y: Float):
+case class Vector2(x: Double, y: Double):
 
+    /**retunrns sum of two vectors as Vector2*/
     def +(other: Vector2) : Vector2 =
         Vector2(x + other.x, y + other.y)
     
+    /**returns differarce of two vectors as Vector2*/
     def -(other: Vector2) : Vector2 =
         Vector2(x - other.x, y - other.y)
 
-    def *(f :Float) : Vector2 =
+    /**@return Vector2 scaled by a Double*/
+    def *(f :Double) : Vector2 =
         Vector2(x * f, y * f)
     
-    def *:(f :Float) : Vector2 =
+    /**@return Vector2 scaled by a Double
+     * 
+     * Made to mimic how vectors are writen in math
+    */
+    def *:(f :Double) : Vector2 =
         Vector2(x * f, y * f)
     
-    def /(f :Float) : Vector2 =
+    /**@return Vector2 scaled by 1/Double*/
+    def /(f :Double) : Vector2 =
         Vector2(x / f, y / f)
     
-    def ->(other: Vector2) : Vector2 =
+    /**Creates vector from point to point*/
+    def -> (other: Vector2) : Vector2 =
         other - this
     
+    /**The length of the vector*/
     lazy val length : Double = 
         math.sqrt(x*x + y*y)
+    
+    /**A vector in the same direction with lenght 1*/
+    lazy val normalized : Vector2 =
+        this / length
+
 
     
